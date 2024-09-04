5e96aaf8c9f6286dcdd4f07db07440e6cce87799
Skanda <57634365+sskanda@users.noreply.github.com>
2024-09-05 01:32:34 +0530

    Update app.py - add port to deploy

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pickle
@@ -80,4 +81,5 @@ def status():
     return jsonify({'message': 'Service is operational'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Get the port from environment variable
+    app.run(host='0.0.0.0', port=port, debug=True)
